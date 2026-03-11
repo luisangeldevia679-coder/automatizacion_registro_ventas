4497fc12382994e41cb3b541e825f3cbbb2f7398
Luis Devia <luisangeldevia679@gmail.com>
2026-03-11 15:21:47 -0500

    adelanto de la rama

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-def register_sale():
-    print("Registering a sale...")
+from modules.register import register_sale 
+from modules.calculations import summarize_sales
+from modules.summary import show_summary
+
+def main():
+    print("Sales system started")
+
+    sales_list = []
+    continue_registering = "yes"
+    while continue_registering == "yes":
+        sale = register_sale()
+        sales_list.append(sale)
+
+        continue_registering = input("Do you want to register another sale? (yes/no): ").lower()
+    product_summary, total_income = summarize_sales(sales_list)
+    show_summary(product_summary, total_income)
+if __name__ == "__main__":
+    main()
+
+        
