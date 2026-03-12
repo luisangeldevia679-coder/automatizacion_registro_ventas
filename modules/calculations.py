3074b63922710d0d06064b1937edb9ada0297a6d
Luis Devia <luisangeldevia679@gmail.com>
2026-03-12 14:05:50 -0500

    calculator arrangement

--- modules/calculations.py
+++ modules/calculations.py
@@ -1,21 +1,13 @@
-def calculate_sale_total(price, quantity):
-    return price * quantity
-
 def summarize_sales(sales_list):
-
     product_summary = {}
     total_income = 0
 
     for sale in sales_list:
         product = sale["product"]
-        price = sale["price"]
-        quantity = sale["quantity"]
-
-        total_income += calculate_sale_total(price, quantity)
-
-        if product not in product_summary:
-            product_summary[product] += quantity
-        else:
-            product_summary[product] = quantity
+        price = int(sale["price"])
+        quantity = int(sale["quantity"])
+        total = price * quantity
+        product_summary[product] = product_summary.get(product, 0) + quantity
+        total_income = total_income + total
 
     return product_summary, total_income
